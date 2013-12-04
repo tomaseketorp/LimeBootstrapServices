869ea8495329c0595a16f7bb26da3f1495ca80f9
Filip Persson <filip.persson@me.com>
2013-12-04 16:00:51 +0100

    First to commit to the appstore build

---
+++ AppStore/js/lbsmanual.js
@@ -0,0 +1,47 @@
+var lbsappstore = {
+	init : function(){
+		$.getJSON("http://limebootstrap.lundalogik.com/API/apps/", function(data) { 
+			console.log(data);
+			var vm = new viewModel(data);
+			console.log(ko.mapping.toJS(vm));
+			ko.applyBindings(vm);
+		});
+	}
+};
+
+/**
+ViewModel
+*/
+var viewModel = function(rawData){
+	var self = this;
+	rawData =	listToMatrix(rawData.apps, 3);
+	console.log(rawData);
+	this.data = ko.mapping.fromJS(rawData);
+}
+
+function listToMatrix(list, elementsPerSubArray) {
+    var matrix = [], i, k;
+
+    for (i = 0, k = -1; i < list.length; i++) {
+        if (i % elementsPerSubArray === 0) {
+            k++;
+            matrix[k] = [];
+        }
+
+        matrix[k].push(list[i]);
+    }
+
+    return matrix;
+}
+
+/**
+Lets get this party on the road
+*/
+$(function(){
+	lbsappstore.init();
+});
+
+
+
+
+
