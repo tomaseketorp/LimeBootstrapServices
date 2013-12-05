298ecc830c211d1d0c16a0257747186241888d3b
Filip Persson <filip.persson@me.com>
2013-12-05 14:01:02 +0100

    Updated appstore

--- AppStore/js/lbsmanual.js
+++ AppStore/js/lbsmanual.js
@@ -1,7 +1,6 @@
 var lbsappstore = {
 	init : function(){
 		$.getJSON("http://limebootstrap.lundalogik.com/API/apps/", function(data) { 
-			console.log(data);
 			var vm = new viewModel(data);
 			console.log(ko.mapping.toJS(vm));
 			ko.applyBindings(vm);
@@ -14,6 +13,11 @@ ViewModel
 */
 var viewModel = function(rawData){
 	var self = this;
+	$(rawData).each(function(app){
+		app.readme = window.atob(app.readme);
+
+	});
+
 	rawData =	listToMatrix(rawData.apps, 3);
 	console.log(rawData);
 	this.data = ko.mapping.fromJS(rawData);
@@ -21,16 +25,13 @@ var viewModel = function(rawData){
 
 function listToMatrix(list, elementsPerSubArray) {
     var matrix = [], i, k;
-
     for (i = 0, k = -1; i < list.length; i++) {
         if (i % elementsPerSubArray === 0) {
             k++;
             matrix[k] = [];
         }
-
         matrix[k].push(list[i]);
     }
-
     return matrix;
 }
 
