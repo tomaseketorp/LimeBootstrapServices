0f1bdd90e0bf79ff36a9e946b2c23bfe36d4d11a
Filip Persson <filip.persson@me.com>
2013-12-05 14:00:44 +0100

    Change readme to type raw

--- browseGitHub.py
+++ browseGitHub.py
@@ -37,7 +37,8 @@ class GitHubConnectorAppStore:
 
                         #Load README for a app
                         readme = requests.get(self.baseURL + "/contents/" + appname + "/" + "README.md?ref=master",
-                                              auth=(self.github_user, self.github_password))
+                                              auth=(self.github_user, self.github_password),
+											  headers={"Accept": "application/vnd.github.VERSION.raw"})
                         if readme.ok:
                             json_readme = json.loads(readme.text)
                             return_json["apps"][j].update({"readme": json_readme["content"]})
